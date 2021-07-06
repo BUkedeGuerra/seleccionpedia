b4b27b94ae88d569a849e3d0d4275a3c72ed86b7
BUkedeGuerra <sarocu666@gmail.com>
2021-07-06 17:30:04 +0200

    primera version

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+console.log("Hello");
+
+const links = document.querySelectorAll("[href^='/']:not([href^='//'])");
+console.log(links);
+
+for (const link of links) {
+  const ruta = link.getAttribute("href");
+  console.log(ruta);
+  link.setAttribute("href", "http://wikipedia.org" + ruta);
+}
+
+const enlaces = document.querySelectorAll("a");
+console.log(enlaces);
+for (const enlace of enlaces) {
+  enlace.setAttribute("target", "_blank");
+}
+
+const inicio = document.querySelector(".contenedormovimiento");
+console.log(inicio);
+
+function movimiento(elemento) {
+  console.log(elemento.innerHTML.split(""));
+}
+
+movimiento(inicio);
+
+function getData(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+async function imagenesfondo() {
+  const sections = document.querySelectorAll("section.section");
+  const imagenes = await getData(
+    `https://api.unsplash.com/photos/random?query=soccer&count=${sections.length}&client_id=kghnmDRzhGa5m6V1Sg4v7W0r0BgX4JOvSdnOnApn4ok`
+  );
+  for (let i = 0; i < sections.length; i++) {
+    console.log(imagenes[i].urls.raw);
+    const image = imagenes[i].urls.raw;
+    sections[i].style.backgroundImage = `url(${image})`;
+  }
+}
+
+/* imagenesfondo(); */
